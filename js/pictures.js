284fe40f1b62d8344d2bc05ee370f44c55ec8294
Ekaterina Chernikova <asstra01@yandex.ru>
2023-07-08 12:27:54 +0700

    Добавляет функцию отрисовки изображений

--- js/pictures.js
+++ js/pictures.js
@@ -1,19 +1,24 @@
 import {randomDescription} from './data.js';
 
-const picturesContainer = document.querySelector('.pictures');
-const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const picturesContainerElement = document.querySelector('.pictures');
+const pictureTemplateElement = document.querySelector('#picture').content.querySelector('.picture');
 
 const otherPictures = randomDescription();
 
-const otherPicturesFragment = document.createDocumentFragment();
+const renderPictures = () => {
 
-otherPictures.forEach(({url, description, likes, comments}) => {
-  const pictureElement = pictureTemplate.cloneNode(true);
-  pictureElement.querySelector('.picture__img').src = url;
-  pictureElement.querySelector('.picture__img').alt = description;
-  pictureElement.querySelector('.picture__likes').textContent = likes;
-  pictureElement.querySelector('.picture__comments').textContent = comments.length;
-  otherPicturesFragment.appendChild(pictureElement);
-});
+  const otherPicturesFragment = document.createDocumentFragment();
 
-picturesContainer.appendChild(otherPicturesFragment);
+  otherPictures.forEach(({ url, description, likes, comments }) => {
+    const pictureElement = pictureTemplateElement.cloneNode(true);
+    pictureElement.querySelector('.picture__img').src = url;
+    pictureElement.querySelector('.picture__img').alt = description;
+    pictureElement.querySelector('.picture__likes').textContent = likes;
+    pictureElement.querySelector('.picture__comments').textContent = comments.length;
+    otherPicturesFragment.appendChild(pictureElement);
+  });
+
+  picturesContainerElement.appendChild(otherPicturesFragment);
+};
+
+export {renderPictures};
