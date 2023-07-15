a228f0e3487c6847f2feea7e1058c1e544f4fdad
Ekaterina Chernikova <asstra01@yandex.ru>
2023-07-15 15:22:56 +0700

    Добавляет функцию загрузки комментариев

--- js/picturesFull.js
+++ js/picturesFull.js
@@ -2,6 +2,7 @@ import {isEscapeKey} from './util.js';
 
 const bigPictureElement = document.querySelector('.big-picture');
 const bigPictureCloseElement = bigPictureElement.querySelector('.big-picture__cancel');
+const commentsLoader = bigPictureElement.querySelector('.comments-loader');
 const commentsList = document.querySelector('.social__comments');
 const commentTemplateElement = document.querySelector('#social__comment').content.querySelector('.social__comment');
 const body = document.querySelector('body');
@@ -22,7 +23,31 @@ const generateComments = (comments) => {
     commentElement.querySelector('.social__picture').src = comments[i].avatar;
     commentElement.querySelector('.social__picture').alt = comments[i].name;
     commentElement.querySelector('.social__text').textContent = comments[i].message;
+    if (i > 4) {
+      commentElement.classList.add('hidden');
+    }
+    if (comments.length <= 5) {
+      commentsLoader.classList.add('hidden');
+    } else {
+      commentsLoader.classList.remove('hidden');
+    }
     commentsList.appendChild(commentElement);
+  }
+};
+
+const loadComments = (comments, socialComments, n, commentCount) => {
+  if (n + 5 <= comments.length) {
+    commentCount.textContent = n + 5;
+    commentsLoader.classList.remove('hidden');
+    for (let i = n; i < n + 5; i++) {
+      socialComments[i].classList.remove('hidden');
+    }
+  } else {
+    for (let i = n; i < comments.length; i++) {
+      socialComments[i].classList.remove('hidden');
+    }
+    commentCount.textContent = comments.length;
+    commentsLoader.classList.add('hidden');
   }
 };
 
@@ -36,6 +61,20 @@ const openBigPicture = (url, likes, comments,description) => {
   body.classList.add('modal-open');
   bigPictureCloseElement.addEventListener('click', closeBigPicture);
   document.addEventListener('keydown', onDocumentKeydown);
+  const socialComments = bigPictureElement.querySelectorAll('.social__comment');
+  const commentCount = bigPictureElement.querySelector('.comments-count-shown');
+  if (comments.length < 5) {
+    commentCount.textContent = comments.length;
+  } else {
+    commentCount.textContent = 5;
+  }
+  let n = 0;
+  commentsLoader.addEventListener('click', () => {
+    if (comments.length > 5) {
+      n += 5;
+      loadComments(comments, socialComments, n, commentCount);
+    }
+  });
 };
 
 function closeBigPicture () {
@@ -44,7 +83,6 @@ function closeBigPicture () {
   bigPictureCloseElement.removeEventListener('click', closeBigPicture);
   document.removeEventListener('keydown', onDocumentKeydown);
 }
-
 
 export {openBigPicture};
 
