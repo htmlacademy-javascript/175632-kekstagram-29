2aa2f16a03a31c2210c0af4afd9ea908dea1ebfc
Ekaterina Chernikova <asstra01@yandex.ru>
2023-07-22 19:09:51 +0700

    Добавляет проверку для валидации хэштега

--- js/form.js
+++ js/form.js
@@ -7,6 +7,88 @@ const body = document.body;
 const imgUploadCancel = document.querySelector('.img-upload__cancel');
 const hashtags = imgUploadForm.querySelector('.text__hashtags');
 const imgUploadComments = imgUploadForm.querySelector('.text__description');
+const scaleControlSmaller = imgUploadForm.querySelector('.scale__control--smaller');
+const scaleControlBigger = imgUploadForm.querySelector('.scale__control--bigger');
+const scaleControlValue = imgUploadForm.querySelector('.scale__control--value');
+const imgUploadPreview = imgUploadForm.querySelector('.img-upload__preview');
+const sliderElement = imgUploadForm.querySelector('.effect-level__slider');
+const effectLevel = imgUploadForm.querySelector('.effect-level__value');
+const effectsRadio = imgUploadForm.querySelectorAll('.effects__radio');
+
+const EFFECTS_CSS = [
+  {name: 'grayscale', min: 0, max: 1, step: 0.1},
+  {name: 'sepia', min: 0, max: 1, step: 0.1},
+  {name: 'invert', min: 0, max: 100, step: 1},
+  {name: 'blur', min: 0, max: 3, step: 0.1},
+  {name: 'brightness', min: 1, max: 3, step: 0.1}
+];
+
+const EFFECTS_NAMES = [
+  'chrome',
+  'sepia',
+  'marvin',
+  'phobos',
+  'heat'
+];
+
+effectsRadio.forEach((effectRadio) => {
+  effectRadio.addEventListener('click', () => {
+    const effect = effectRadio.value;
+    applyEffect(effect);
+  });
+}
+);
+
+noUiSlider.create(sliderElement, {
+  range: {
+    min: 0,
+    max: 1,
+  },
+  start: effectLevel.value,
+  step: 0.1,
+  connect: 'lower',
+});
+
+function applyEffect (effect) {
+  const index = EFFECTS_NAMES.indexOf(effect);
+  const effectCss = EFFECTS_CSS[index].name;
+  effectLevel.value = EFFECTS_CSS[index].max;
+  sliderElement.noUiSlider.updateOptions ({
+    range: {
+      min: EFFECTS_CSS[index].min,
+      max: EFFECTS_CSS[index].max,
+    },
+    start: effectLevel.value,
+    step: EFFECTS_CSS[index].step,
+    connect: 'lower',
+  });
+  sliderElement.noUiSlider.on('update', () => {
+    effectLevel.value = sliderElement.noUiSlider.get();
+    imgUploadPreview.style.filter = `${effectCss }(${ effectLevel.value })`;
+  });
+}
+
+scaleControlBigger.addEventListener('click', () => {
+  let scaleControlNumber = parseInt(scaleControlValue.value, 10);
+  if (scaleControlNumber < 100) {
+    if (scaleControlNumber >= 0) {
+      scaleControlNumber += 25;
+      scaleControlValue.value = `${scaleControlNumber}%`;
+      imgUploadPreview.style.transform = `scale(${ scaleControlNumber / 100 })`;
+    }
+  }
+});
+
+scaleControlSmaller.addEventListener('click', () => {
+  let scaleControlNumber = parseInt(scaleControlValue.value, 10);
+  if (scaleControlNumber > 0) {
+    if (scaleControlNumber <= 100) {
+      scaleControlNumber -= 25;
+      scaleControlValue.value = `${scaleControlNumber}%`;
+      imgUploadPreview.style.transform = `scale(${ scaleControlNumber / 100 })`;
+    }
+  }
+});
 
 const getArray = (value) => value.split(' ');
 
@@ -54,11 +136,15 @@ const regHashtag = /^#[a-zа-яё0-9]{1,19}$/i;
 function validateHashtag (value) {
   let n = true;
   const hashtagsArray = getArray(value);
-  hashtagsArray.forEach((element) => {
-    if(!regHashtag.test(element)) {
-      n = false;
-    }
-  });
+  if (hashtagsArray === ['']) {
+    n = true;
+  } else {
+    hashtagsArray.forEach((element) => {
+      if (!regHashtag.test(element)) {
+        n = false;
+      }
+    });
+  }
   return n;
 }
 
